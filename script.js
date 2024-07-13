ba68bbb5b68d86de10d8953584de1a6145c833d9
papp <116855015+p-a-p-p@users.noreply.github.com>
2024-07-13 19:28:16 +0800

    added reset button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let num;
+let size;
+
 function createGrid(num) {
   const container = document.querySelector(".container");
   const boxSize = 100 / num + "%";
@@ -21,10 +24,9 @@ function createButton() {
   const button = document.createElement("button");
   button.className = "adjustButton";
   button.textContent = "ADJUST GRID SIZE";
-
+  container = document.querySelector(".container");
+  //adjust grid size
   button.addEventListener("click", function () {
-    container = document.querySelector(".container");
-
     size = prompt("Adjust the size of the grid to?");
 
     if (size <= 100 && size > 0) {
@@ -34,8 +36,19 @@ function createButton() {
       alert("Only Input Integer from 1 - 100");
     }
   });
-
   inputContainer.appendChild(button);
+
+  //reset grid
+  const button2 = document.createElement("button");
+  button2.className = "eraseButton";
+  button2.textContent = "ERASE";
+
+  button2.addEventListener("click", function () {
+    container.textContent = "";
+    createGrid(size);
+  });
+
+  inputContainer.appendChild(button2);
 }
 
 function randomColor() {
